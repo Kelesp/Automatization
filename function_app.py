4bdee2109faca8bb3291c96d6825184bb01d69aa
Kevin Espinosa Londoño <kelesp.estudio@gmail.com>
2025-06-02 18:04:02 -0500

    Nueva actualizacion en el puerto

--- function_app.py
+++ function_app.py
@@ -222,4 +222,4 @@ Texto a analizar:
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("function_app:app", host="0.0.0.0", port=8000, workers=1)
+    uvicorn.run("function_app:app", host="0.0.0.0", port=8002, workers=1)
